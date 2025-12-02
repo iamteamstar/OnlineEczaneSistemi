229c11ac17a39bc06a29ade9f40a8ef6e5483488
iamteamstar <azadkocak29@gmail.com>
2025-12-02 23:58:54 +0300

    admin index sayfasi olusturuldu

--- OnlineEczaneSistemi/obj/Debug/net8.0/OnlineEczaneSistemi.AssemblyInfo.cs
+++ OnlineEczaneSistemi/obj/Debug/net8.0/OnlineEczaneSistemi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ef42d01e7a46d6412dab6788f352b5cca0183b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10df86296702e81e13c4cf0f46911e8674a90a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
