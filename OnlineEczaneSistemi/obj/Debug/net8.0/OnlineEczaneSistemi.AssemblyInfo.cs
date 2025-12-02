048491aeb8900d376820775b59a6c674bed73e43
iamteamstar <azadkocak29@gmail.com>
2025-12-02 20:33:35 +0300

    Migration eklendi

--- OnlineEczaneSistemi/obj/Debug/net8.0/OnlineEczaneSistemi.AssemblyInfo.cs
+++ OnlineEczaneSistemi/obj/Debug/net8.0/OnlineEczaneSistemi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508e53f662656623e7f7d7c3c6f7bb62dfc9dd7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f123dacd9d4422b2f01c513f16cd66f3678571c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
