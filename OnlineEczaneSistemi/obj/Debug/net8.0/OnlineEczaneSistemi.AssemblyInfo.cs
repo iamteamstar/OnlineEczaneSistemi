6f55515a6ebd8029921c86f42c484f43f7c13442
iamteamstar <azadkocak29@gmail.com>
2025-12-03 04:31:03 +0300

    eczane yonetim paneline erisim saglandi

--- OnlineEczaneSistemi/obj/Debug/net8.0/OnlineEczaneSistemi.AssemblyInfo.cs
+++ OnlineEczaneSistemi/obj/Debug/net8.0/OnlineEczaneSistemi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c6ad91e3fc91aa10e7868d684e3abee8132bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357e54db3bb31c780e51ee39cd59cfb77907430e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineEczaneSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
